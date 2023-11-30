b4e4001a2b7728f4ac52a523ffb55ad4b8294eef
Hung Do <nguyenhung_do@hotmail.com>
2023-11-30 13:08:40 -0500

    Found extremas for all octaves

--- Local_Maximas.m
+++ Local_Maximas.m
@@ -2,8 +2,12 @@
 clear all;
 
 im_left = double(imread('bikepath_left_resized.jpg'))/255; 
+im_left = imresize(im_left, 0.6); 
+% im_left = double(imread('sample_left.jpg'))/255; 
 
 im_right = double(imread('bikepath_right_resized.jpg'))/255; 
+im_right = imresize(im_right, 0.6);
+% im_right = double(imread('sample_right.jpg'))/255; 
 
 %% Grayscale
 gray_left = rgb2gray(im_left);
@@ -21,9 +25,13 @@ for row=1:size(scales,1)
 end
 
 %% Downsampled left
+% Find maximas for all octaves and superimpose on the 1st octave (3 x 3)
 tmp_iml = gray_left(:,:);
 tmp_filter = gray_left(:,:);
 windowSize = int32(size(gray_left, 2) / 50);
+
+maximas = [];
+stdev_s = [];
 
 hold on
 for oct=1:4
@@ -37,9 +45,6 @@ for oct=1:4
             tmp_filter = imgaussfilt(tmp_iml, sigma, FilterSize=kern);
             
             DoG = tmp_iml - tmp_filter;
-            if col == 1
-                DoG_1 = DoG;
-            end
         end
     else
         tmp_iml = tmp_iml(1:2:end,1:2:end);
@@ -51,50 +56,40 @@ for oct=1:4
             end
             tmp_filter = imgaussfilt(tmp_iml, sigma, FilterSize=kern);
             DoG = tmp_iml - tmp_filter;
-            
-            if col == 1
-                if oct == 2
-                    DoG_2 = DoG;
-                elseif oct == 3
-                    DoG_3 = DoG;
-                else
-                    DoG_4 = DoG;
-                end
-            end
-          
-        end 
+       end 
     end
-end
 
-%% Find local maximas
-maximas = [];
+    % Find local maximas
+    for r = 1:size(DoG, 1)
+        for c = 1:size(DoG, 2)
+            if mod(r, windowSize) == 1 && mod(c, windowSize) == 1
+                if r + (windowSize - 1) > size(DoG, 1)
+                    end_r = size(DoG, 1);
+                else
+                    end_r = r + (windowSize - 1);
+                end
+    
+                if c + (windowSize - 1) > size(DoG, 2)
+                    end_c = size(DoG, 2);
+                else
+                    end_c = c + (windowSize - 1);
+                end
+    
+                temp = DoG(r:end_r, c:end_c);
+                
+                % Calculate std here 
+                stdev = std(temp(:));
+                stdev_s(end + 1) = stdev;
+    
+                [val, index] = max(temp, [], "all");
+                [maxima_r, maxima_c] = ind2sub(size(temp), index);
+    
+                maximas = cat(1, maximas, [(r + (maxima_r - 1))* 2^(oct - 1), (c + (maxima_c - 1))* 2^(oct - 1)]);
 
-for r = 1:size(DoG_1, 1)
-    for c = 1:size(DoG_1, 2)
-        if mod(r, windowSize) == 1 && mod(c, windowSize) == 1
-            if r + (windowSize - 1) > size(DoG_1, 1)
-                end_r = size(DoG_1, 1);
-            else
-                end_r = r + (windowSize - 1);
             end
-
-            if c + (windowSize - 1) > size(DoG_1, 2)
-                end_c = size(DoG_1, 2);
-            else
-                end_c = c + (windowSize - 1);
-            end
-
-            temp = DoG_1(r:end_r, c:end_c);
-
-            [val, index] = max(temp, [], "all");
-            [maxima_r, maxima_c] = ind2sub(size(temp), index);
-            maximas = cat(1, maximas, [r + (maxima_r - 1), c + (maxima_c - 1)]);
-
         end
     end
 end
-
-% maximas = unique(sort(maximas, 2), 'rows');
 
 %% Find edge pixels
 edge_left = edge(gray_left);
@@ -109,18 +104,22 @@ for r = 1:size(edge_left, 1)
 end
 
 %% Display all extrema points
-% gray_left_maximas = gray_left(:, :);
-% for i = 1:size(maximas, 1)
-%     gray_left_maximas = insertShape(gray_left_maximas, "circle", [maximas(i, 2),maximas(i, 1), 5], ShapeColor=['red'], Opacity=1);
-% end
+gray_left_maximas = gray_left(:, :);
+for i = 1:size(maximas, 1)
+    gray_left_maximas = insertShape(gray_left_maximas, "circle", [maximas(i, 2),maximas(i, 1), 5], ShapeColor=['red'], Opacity=1);
+end
 
-% imshow(gray_left_maximas);
+subplot(1, 2, 1), imshow(gray_left_maximas), title('All Extremas');
 
 %% Display pruned extrema points
 gray_left_pruned = gray_left(:, :);
 is_edge = ismember(maximas, edge_pixels, 'rows');
+% Need to play around with this more.
+threshold = mean(stdev_s) + std(stdev_s);
+
 count_edge = 0;
 count_border = 0;
+count_std = 0;
 
 for i = 1:size(maximas, 1)
     if maximas(i, 1) <= int32(windowSize / 2) || maximas(i, 2) <= int32(windowSize / 2) || maximas(i, 1) > size(gray_left_pruned, 1) - int32(windowSize / 2) || maximas(i, 2) > size(gray_left_pruned, 2) - int32(windowSize / 2)
@@ -129,9 +128,12 @@ for i = 1:size(maximas, 1)
     elseif is_edge(i) == 1
         count_edge = count_edge + 1;
         continue
+    elseif stdev_s(i) < threshold
+        count_std = count_std + 1;
+        continue
     end
-    
+
     gray_left_pruned = insertShape(gray_left_pruned, "circle", [maximas(i, 2),maximas(i, 1), 5], ShapeColor=['red'], Opacity=1);
 end
 
-imshow(gray_left_pruned);
+subplot(1, 2, 2), imshow(gray_left_pruned), title('Pruned Extremas');
