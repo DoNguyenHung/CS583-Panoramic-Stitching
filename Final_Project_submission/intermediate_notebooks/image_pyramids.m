571da488c437ae587063aa06159f59f6790e964f
RamonaRubalcava <43584091+RamonaRubalcava@users.noreply.github.com>
2023-12-11 21:44:37 -0500

    intermediate notebooks folder
    folder has the intermediate notebooks

---
+++ Final_Project_submission/intermediate_notebooks/image_pyramids.m
@@ -0,0 +1,113 @@
+%% Reading in the image file
+clear all;
+
+im_left = double(imread('bikepath_left_resized.jpg'))/255; 
+%f1 = figure('Name', 'Bike Path Left');
+%imshow(im_left)
+
+im_right = double(imread('bikepath_right_resized.jpg'))/255; 
+%f2 = figure('Name', 'Bike Path Right');
+%imshow(im_right)
+
+%% Grayscale
+
+gray_left = rgb2gray(im_left);
+gray_right = rgb2gray(im_right);
+
+%f3 = figure('Name', 'Bike Path Grays');
+%subplot(1,2,1), imshow(gray_left);
+%subplot(1,2,2), imshow(gray_right);
+
+%% Calculate Scales
+init_sig = 1.6; 
+scales = zeros(4,5);
+
+%auto calculate scales
+for row=1:size(scales,1)
+    for col=1:size(scales,2)
+        scales(row, col) = (2^(row-1))*(sqrt(2)^(col-1))*init_sig;
+    end
+end
+
+%% Downsampled left
+
+tmp_iml = gray_left(:,:);
+tmp_filter = gray_left(:,:);
+
+f1 = figure('Name', 'Bike Path Image Pyramid');
+
+hold on
+pos = 1;
+for oct=1:4
+    if oct==1
+        for col=1:size(scales,2)
+            sigma = scales(oct, col); 
+            kern = ceil(3*sigma);
+            if rem(kern,2)==0
+                kern = floor(3*sigma);
+            end
+            tmp_filter = imgaussfilt(tmp_iml, sigma, FilterSize=kern);
+            DoG = tmp_iml - tmp_filter;
+
+            subplot(4,5,pos), imshow(tmp_filter), axis on;
+            pos = pos+1;
+            
+            %imwrite(tmp_filter, sprintf('./im_pyramids_bikeleft/GAUSoct%d_scale%d.jpg', oct, col));
+            %imwrite(DoG, sprintf('./im_pyramids_bikeleft/DoG_oct%d_scale%d.jpg', oct, col));
+        end 
+    else
+        tmp_iml = tmp_iml(1:2:end,1:2:end);
+        for col=1:size(scales,2) 
+            sigma = scales(oct, col); 
+            kern = ceil(3*sigma);
+            if rem(kern,2)==0
+                kern = floor(3*sigma);
+            end
+            tmp_filter = imgaussfilt(tmp_iml, sigma, FilterSize=kern);
+            DoG = tmp_iml - tmp_filter;
+             
+            subplot(4,5,pos), imshow(tmp_filter), axis on;
+            pos = pos+1;
+          
+            %imwrite(tmp_filter, sprintf('./im_pyramids_bikeleft/GAUSoct%d_scale%d.jpg', oct, col));
+            %imwrite(DoG, sprintf('./im_pyramids_bikeleft/DoG_oct%d_scale%d.jpg', oct, col));
+        end 
+    end
+end
+
+hold off
+saveas(f1, 'image_pyramids.jpg')
+
+%% Downsampled right
+
+tmp_imr = gray_right(:,:);
+tmp_filt = gray_right(:,:);
+
+for oct=1:4
+    if oct==1
+        for col=1:size(scales,2)
+            sigma = scales(oct, col); 
+            kern = ceil(3*sigma);
+            if rem(kern,2)==0
+                kern = floor(3*sigma);
+            end
+            tmp_filt = imgaussfilt(tmp_imr, sigma, FilterSize=kern);
+            DoG = tmp_imr - tmp_filt;
+            %imwrite(tmp_filt, sprintf('./im_pyramids_bikeright/GAUSoct%d_scale%d.jpg', oct, col));
+            %imwrite(DoG, sprintf('./im_pyramids_bikeright/DoG_oct%d_scale%d.jpg', oct, col));
+        end 
+    else
+        tmp_imr = tmp_imr(1:2:end,1:2:end);
+        for col=1:size(scales,2) 
+            sigma = scales(oct, col); 
+            kern = ceil(3*sigma);
+            if rem(kern,2)==0
+                kern = floor(3*sigma);
+            end
+            tmp_filt = imgaussfilt(tmp_imr, sigma, FilterSize=kern);
+            DoG = tmp_imr - tmp_filt;
+            %imwrite(tmp_filt, sprintf('./im_pyramids_bikeright/GAUSoct%d_scale%d.jpg', oct, col));
+            %imwrite(DoG, sprintf('./im_pyramids_bikeright/DoG_oct%d_scale%d.jpg', oct, col));
+        end 
+    end
+end
---
+++ Final_Project_submission/intermediate_notebooks/point_correspondences.m
@@ -0,0 +1,68 @@
+%% Reading in the image file
+clear all;
+
+ogleft = double(imread('bikepath_left.jpg'))/255; 
+%f1 = figure('Name', 'Bike Path Left');
+%imshow(ogleft)
+
+ogright = double(imread('bikepath_right.jpg'))/255; 
+%f2 = figure('Name', 'Bike Path Right');
+%imshow(ogright)
+
+%% Resize to smaller scale for processing
+
+left = imresize(ogleft, 0.25); 
+%f3 = figure('Name', 'Bike Path Left scaled down');
+%imshow(left)
+
+right = imresize(ogright, 0.25);
+%f4 = figure('Name', 'Bike Path Right scaled down');
+%imshow(right)
+
+%% Point Correspondence with insert 
+
+% uncomment these for the original left size (X,Y)
+%branchlft= [3800 978];
+%rocklft= [3413 3313];
+%signlft= [2388 2919];
+%leaflft= [4174 5088];
+
+% uncomment these for the original left size (X,Y)
+%branchrgt= [2422 1275];
+%rockrgt= [2040 3400];
+%signrgt= [2422 1275];
+%leafrgt= [2512 5029];
+
+% the resized locations left (X,Y)
+branchlft= [950 245];
+rocklft= [853 829];
+signlft= [598 730];
+leaflft= [1044 1273];
+
+% the resized locations right (X,Y)
+branchrgt= [606 319];
+rockrgt= [511 850];
+signrgt= [259 750];
+leafrgt= [628 1258];
+
+withptslft = insertShape(left, "filled-circle", [branchlft(1),branchlft(2),10], ShapeColor=['red'], Opacity=1);
+withptslft = insertShape(withptslft, "filled-circle", [rocklft(1),rocklft(2),10], ShapeColor=['magenta'], Opacity=1);
+withptslft = insertShape(withptslft, "filled-circle", [signlft(1),signlft(2),10], ShapeColor=['green'], Opacity=1);
+withptslft = insertShape(withptslft, "filled-circle", [leaflft(1),leaflft(2),10], ShapeColor=['cyan'], Opacity=1);
+%f6 = figure('Name', 'Bike Path Left with Points');
+%imshow(withptslft)
+
+withptsrgt = insertShape(right, "filled-circle", [branchrgt(1),branchrgt(2),10], ShapeColor=['red'], Opacity=1);
+withptsrgt = insertShape(withptsrgt, "filled-circle", [rockrgt(1),rockrgt(2),10], ShapeColor=['magenta'], Opacity=1);
+withptsrgt = insertShape(withptsrgt, "filled-circle", [signrgt(1),signrgt(2),10], ShapeColor=['green'], Opacity=1);
+withptsrgt = insertShape(withptsrgt, "filled-circle", [leafrgt(1),leafrgt(2),10], ShapeColor=['cyan'], Opacity=1);
+%f7 = figure('Name', 'Bike Path Right with Points');
+%imshow(withptsrgt)
+
+f8 = figure('Name', 'Together');
+together = [withptslft, withptsrgt];
+imshow(together);
+
+%imwrite(withptslft, 'bikepathleft_wpts.jpg')
+%imwrite(withptsrgt, 'bikepathright_wpts.jpg')
+%imwrite(together, 'bikepath_wpts_together.jpg')
